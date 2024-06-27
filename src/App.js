bb9adb324a60cd2ceaec94e627af34264d0bd118
antoineTsinga <antoinetsinga@gmail.com>
2024-06-27 03:29:31 +0200

    add analytics

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { NotificationContainer } from "react-notifications";
 import { AppContextProvider } from "./AppContext";
 
 import Router from "./Router";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <AppContextProvider>
         <NotificationContainer />
         <Router />
